eb81e556b08ab215a59442e9928633936d848cdd
nahidahassan <nahidahassan220@gmail.com>
2024-05-21 17:42:37 +0500

    params

--- src/app/profile/[id]/post/[postid]/page.tsx
+++ src/app/profile/[id]/post/[postid]/page.tsx
@@ -3,8 +3,11 @@ import { useParams } from "next/navigation"
 
 export default function Profile(){
     const params=useParams();
+    console.log("id")
     console.log(params.id)
+    console.log("postid")
+    console.log(params.postid)
     return(
-        <div>My Id profile page. </div>
+        <div>My Id profile postid page. </div>
     )
 }
