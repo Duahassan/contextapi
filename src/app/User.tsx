f5e12dd3c29b0e38a27c4196ad12dfc4c44ff5c4
nahidahassan <nahidahassan220@gmail.com>
2024-05-22 15:53:20 +0500

    reusable component

---
+++ src/app/User.tsx
@@ -0,0 +1,9 @@
+const User =(props:any)=>{
+    return(
+
+        <div><h1>Hey i'm user</h1>
+        <div>My name is{props.name}</div>
+        </div>
+    )
+}
+export default User;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import User from "./User";
 
 export default function Home() {
   return (
     <main>
+      <User name="Dua"/>
+      <User name="Ali"/>
+      <User name="Dani"/>
       <div>Hello Contextapi</div>
     </main>
   );
